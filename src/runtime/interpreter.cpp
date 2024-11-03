015be92b983c0ce4b76540e96c32209a04d34333
YellowCat98 <elcucuyisgay@gmail.com>
2024-11-03 16:52:22 +0100

    all casts are guaranteed to be valid casts, so static_cast will do!

--- src/runtime/interpreter.cpp
+++ src/runtime/interpreter.cpp
@@ -35,7 +35,7 @@ std::unique_ptr<values::RuntimeVal> interpreter::evaluate_binary_expr(AST::BinEx
     auto rhs = evaluate(binop->right, env);
 
     if (lhs->type == values::ValueType::Number && rhs->type == values::ValueType::Number) {
-        return evaluate_numeric_binary_expr(std::make_unique<values::NumVal>(*dynamic_cast<values::NumVal*>(lhs.get())), std::make_unique<values::NumVal>(*dynamic_cast<values::NumVal*>(rhs.get())), binop->op);
+        return evaluate_numeric_binary_expr(std::make_unique<values::NumVal>(*static_cast<values::NumVal*>(lhs.get())), std::make_unique<values::NumVal>(*static_cast<values::NumVal*>(rhs.get())), binop->op);
     }
 
     return std::make_unique<values::RuntimeVal>();
@@ -49,7 +49,7 @@ std::unique_ptr<values::RuntimeVal> interpreter::evaluate_object_expr(AST::Objec
     auto object = std::make_unique<values::ObjectVal>();
 
     for (auto& prop : obj->properties) {
-        auto runtimeVal = (prop->value.value() == nullptr) ? env->lookupVar(prop->key) : evaluate(dynamic_cast<AST::Stmt*>(prop->value.value()), env);
+        auto runtimeVal = (prop->value.value() == nullptr) ? env->lookupVar(prop->key) : evaluate(static_cast<AST::Stmt*>(prop->value.value()), env);
 
         object->properties.emplace(prop->key, std::move(runtimeVal));
     }
@@ -65,13 +65,13 @@ std::unique_ptr<values::RuntimeVal> interpreter::evaluate_call_expr(AST::CallExp
     auto fn = evaluate(expr->caller, env);
 
     if (fn->type == values::ValueType::NativeFn) {
-        auto result = dynamic_cast<values::NativeFnValue*>(fn.get())->call(std::move(args), env);
+        auto result = static_cast<values::NativeFnValue*>(fn.get())->call(std::move(args), env);
         return result;
     }
 
     if (fn->type == values::ValueType::Function) {
         
-        auto func = dynamic_cast<values::FunValue*>(fn.get());
+        auto func = static_cast<values::FunValue*>(fn.get());
         auto scope = std::unique_ptr<Environment>(func->decEnv);
 
         for (int i = 0; i < func->params.size(); ++i) {
@@ -101,7 +101,7 @@ std::unique_ptr<values::RuntimeVal> interpreter::evaluate_assignment(AST::Assign
     if (node->assigne->kind != AST::NodeType::Identifier) {
         throw std::runtime_error(fmt::format("Invalid LHS in assignment expression."));
     }
-    auto name = dynamic_cast<AST::Identifier*>(node->assigne)->symbol;
+    auto name = static_cast<AST::Identifier*>(node->assigne)->symbol;
     return env->assignVar(name, evaluate(node->value, env));
 }
 
@@ -116,7 +116,7 @@ std::unique_ptr<values::RuntimeVal> interpreter::evaluate_fun_declaration(AST::F
 }
 
 std::unique_ptr<values::RuntimeVal> interpreter::evaluate_if_statement(AST::IfStmt* ifstmt, Environment* env) {
-    bool condition = dynamic_cast<values::BoolVal*>(evaluate(ifstmt->condition, env).get())->value;
+    bool condition = static_cast<values::BoolVal*>(evaluate(ifstmt->condition, env).get())->value;
 
     bool hasElse = ifstmt->elseStmt.has_value();
 
@@ -147,8 +147,8 @@ std::unique_ptr<values::RuntimeVal> interpreter::evaluate_if_statement(AST::IfSt
 }
 
 std::unique_ptr<values::RuntimeVal> interpreter::evaluate_comparison_expr(AST::CompEx* compEx, Environment* env) {
-    auto left = dynamic_cast<values::NumVal*>(evaluate(compEx->left, env).get());
-    auto right = dynamic_cast<values::NumVal*>(evaluate(compEx->right, env).get());
+    auto left = static_cast<values::NumVal*>(evaluate(compEx->left, env).get());
+    auto right = static_cast<values::NumVal*>(evaluate(compEx->right, env).get());
 
     bool result;
 
@@ -165,7 +165,7 @@ std::unique_ptr<values::RuntimeVal> interpreter::evaluate_comparison_expr(AST::C
 std::unique_ptr<values::RuntimeVal> interpreter::evaluate_member_expr(AST::MemberExpr* member, Environment* env) {
     auto objectVal = evaluate(member->object, env);
 
-    auto propertyIdent = dynamic_cast<AST::Identifier*>(member->property);
+    auto propertyIdent = static_cast<AST::Identifier*>(member->property);
     if (!propertyIdent) {
         throw std::runtime_error("Interpreter: Property in member expression is not an identifier.");
     }
@@ -173,7 +173,7 @@ std::unique_ptr<values::RuntimeVal> interpreter::evaluate_member_expr(AST::Membe
     auto propertyName = propertyIdent->symbol;
 
     if (objectVal->type == values::ValueType::Object) {
-        auto object = dynamic_cast<values::ObjectVal*>(objectVal.get());
+        auto object = static_cast<values::ObjectVal*>(objectVal.get());
 
         auto it = object->properties.find(propertyName);
         if (it != object->properties.end()) {
@@ -197,7 +197,7 @@ std::unique_ptr<values::RuntimeVal> interpreter::evaluate_while_statement(AST::W
     auto lastEvaluated =std::make_unique<values::RuntimeVal>();
 
     while (true) {
-        bool condition = dynamic_cast<values::BoolVal*>(evaluate(whilestmt->condition, env).get())->value;
+        bool condition = static_cast<values::BoolVal*>(evaluate(whilestmt->condition, env).get())->value;
 
         if (!condition) break;
         try {
@@ -217,47 +217,47 @@ std::unique_ptr<values::RuntimeVal> interpreter::evaluate(AST::Stmt* astNode, En
     switch (astNode->kind) {
         case AST::NodeType::NumericLiteral: {
             auto value = std::make_unique<values::NumVal>();
-            value->value = dynamic_cast<AST::NumericLiteral*>(astNode)->value;
+            value->value = static_cast<AST::NumericLiteral*>(astNode)->value;
             return value;
         }
         case AST::NodeType::Identifier: {
-            return evaluate_identifier(dynamic_cast<AST::Identifier*>(astNode), env);
+            return evaluate_identifier(static_cast<AST::Identifier*>(astNode), env);
         }
         case AST::NodeType::BinaryExpr: {
-            return evaluate_binary_expr(dynamic_cast<AST::BinEx*>(astNode), env);
+            return evaluate_binary_expr(static_cast<AST::BinEx*>(astNode), env);
         }
         case AST::NodeType::Program: {
-            return evaluate_program(dynamic_cast<AST::Program*>(astNode), env);
+            return evaluate_program(static_cast<AST::Program*>(astNode), env);
         }
         case AST::NodeType::VarDeclare: {
-            return evaluate_var_declaration(dynamic_cast<AST::VarDeclare*>(astNode), env);
+            return evaluate_var_declaration(static_cast<AST::VarDeclare*>(astNode), env);
         }
         case AST::NodeType::AssignmentExpr: {
-            return evaluate_assignment(dynamic_cast<AST::AssignExpr*>(astNode), env);
+            return evaluate_assignment(static_cast<AST::AssignExpr*>(astNode), env);
         }
         case AST::NodeType::ObjectLiteral: {
-            return evaluate_object_expr(dynamic_cast<AST::ObjectLiteral*>(astNode), env);
+            return evaluate_object_expr(static_cast<AST::ObjectLiteral*>(astNode), env);
         }
         case AST::NodeType::CallExpr: {
-            return evaluate_call_expr(dynamic_cast<AST::CallExpr*>(astNode), env);
+            return evaluate_call_expr(static_cast<AST::CallExpr*>(astNode), env);
         }
         case AST::NodeType::FunctionDeclaration: {
-            return evaluate_fun_declaration(dynamic_cast<AST::FunDeclare*>(astNode), env);
+            return evaluate_fun_declaration(static_cast<AST::FunDeclare*>(astNode), env);
         }
         case AST::NodeType::If: {
-            return evaluate_if_statement(dynamic_cast<AST::IfStmt*>(astNode), env);
+            return evaluate_if_statement(static_cast<AST::IfStmt*>(astNode), env);
         }
         case AST::NodeType::CompExpr: {
-            return evaluate_comparison_expr(dynamic_cast<AST::CompEx*>(astNode), env);
+            return evaluate_comparison_expr(static_cast<AST::CompEx*>(astNode), env);
         }
         case AST::NodeType::MemberExpr: {
-            return evaluate_member_expr(dynamic_cast<AST::MemberExpr*>(astNode), env);
+            return evaluate_member_expr(static_cast<AST::MemberExpr*>(astNode), env);
         }
         case AST::NodeType::StringLiteral: {
-            return evaluate_string(dynamic_cast<AST::StringLiteral*>(astNode), env);
+            return evaluate_string(static_cast<AST::StringLiteral*>(astNode), env);
         }
         case AST::NodeType::While: {
-            return evaluate_while_statement(dynamic_cast<AST::WhileStmt*>(astNode), env);
+            return evaluate_while_statement(static_cast<AST::WhileStmt*>(astNode), env);
         }
         case AST::NodeType::BreakStmt: {
             throw utils::Break();
